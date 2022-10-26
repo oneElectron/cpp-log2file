1dae9893e47bee03e1cbb8e7079c1c3fa852d6ae
oneElectron <one.electron109@protonmail.com>
2022-10-26 22:54:40 +0200

    added vulkan extensions

--- include/log2file/vulkan.hpp
+++ include/log2file/vulkan.hpp
@@ -22,3 +22,47 @@
 #define VkL2Fversion(message)
 #endif
 
+// version
+#ifdef DEBUG
+#define VkL2FsubVersion(vkVersion) {uint32_t funcMajor = 0, funcMinor = 0, funcPatch = 0;\
+  funcMajor = VK_API_VERSION_MAJOR(vkVersion);\
+  funcMinor = VK_API_VERSION_MINOR(vkVersion);\
+  funcPatch = VK_API_VERSION_PATCH(vkVersion);\
+  \
+  std::fstream outputFile;\
+  outputFile.open(L2F_OUTPUT_FILE, std::ios::app);\
+  outputFile << "\t\t" << "version" << ": " << funcMajor << '.' << funcMinor << '.' << funcPatch << std::endl;\
+  outputFile.close();}
+#endif
+#ifndef DEBUG
+#define VkL2Fversion(message)
+#endif
+
+#ifdef DEBUG
+#define VkL2Fextensions(message, vkExtensions) {\
+  std::fstream outputFile;\
+  outputFile.open(L2F_OUTPUT_FILE, std::ios::app);\
+  outputFile << "\t" << message << std::endl;\
+  for (auto& extension : vkExtensions) {\
+    outputFile << "\t\t" << extension.extensionName << std::endl;\
+  }\
+  outputFile.close();}
+#endif
+#ifndef DEBUG
+#define VkL2Fversion(message)
+#endif
+
+#ifdef DEBUG
+#define VkL2FsubExtensions(message, vkExtensions) {\
+  std::fstream outputFile;\
+  outputFile.open(L2F_OUTPUT_FILE, std::ios::app);\
+  outputFile << "\t\t" << message << std::endl;\
+  for (auto& extension : vkExtensions) {\
+    outputFile << "\t\t\t" << extension.extensionName << std::endl;\
+  }\
+  outputFile.close();}
+#endif
+#ifndef DEBUG
+#define VkL2Fversion(message)
+#endif
+
--- tests/vulkan/vulkan.cpp
+++ tests/vulkan/vulkan.cpp
@@ -2,6 +2,7 @@
 #include "include/log2file/log2file.hpp"
 #include "include/log2file/vulkan.hpp"
 #include <vulkan/vulkan.h>
+#include <vector>
 
 int main(){
   L2Fnewline();
@@ -9,4 +10,15 @@ int main(){
   uint32_t version;
   vkEnumerateInstanceVersion(&version);
   VkL2Fversion(version);
+
+  uint32_t instanceExtensionCount = 0;
+
+  std::vector<VkExtensionProperties> instanceExtensions;
+
+  vkEnumerateInstanceExtensionProperties(NULL, &instanceExtensionCount, nullptr);
+  instanceExtensions.resize(instanceExtensionCount);
+  vkEnumerateInstanceExtensionProperties(nullptr, &instanceExtensionCount, instanceExtensions.data());
+
+  VkL2Fextensions("availible instance extensions:", instanceExtensions);
+  VkL2FsubExtensions("availible instance extensions", instanceExtensions);
 }
